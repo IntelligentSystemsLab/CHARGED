ae94c976bca67b487bc6816987b735b3b6c505ed
MatZaharia <guozh29@mail2.sysu.edu.cn>
2025-04-12 13:23:01 +0800

    Update nets.py

--- api/model/nets.py
+++ api/model/nets.py
@@ -100,7 +100,7 @@ class Arima:
 
 
 class Fcnn(nn.Module):
-    def __init__(self, n_fea, node=247, seq=12):  # input_dim = seq_length
+    def __init__(self, n_fea, node, seq=12):  # input_dim = seq_length
         super(Fcnn, self).__init__()
         self.num_feat = n_fea
         self.seq = seq
@@ -120,7 +120,7 @@ class Fcnn(nn.Module):
 
 
 class Lstm(nn.Module):
-    def __init__(self, seq, n_fea, node=331):
+    def __init__(self, seq, n_fea, node):
         super(Lstm, self).__init__()
         self.num_feat = n_fea
         self.nodes = node
@@ -145,4 +145,84 @@ class Lstm(nn.Module):
         lstm_out = lstm_out.reshape(bs, self.nodes, self.seq_len * self.lstm_hidden_dim)
         x = self.linear(lstm_out)
         x = torch.squeeze(x)
-        return x
+        return x  # [batch, node]
+
+
+class SegRNN(nn.Module):
+
+    def __init__(self, seq_len,pred_len,node,n_fea,seg_len=1,d_model=256,dropout=0.1):
+        super(SegRNN, self).__init__()
+
+        # get parameters
+        self.seq_len = seq_len
+        self.enc_in = n_fea
+        self.d_model = d_model
+        self.dropout = dropout
+        self.pred_len = pred_len
+        self.nodes = node
+
+        self.seg_len = seg_len
+        self.seg_num_x = self.seq_len // self.seg_len
+        self.seg_num_y = self.pred_len // self.seg_len
+
+        # building model
+        self.valueEmbedding = nn.Sequential(
+            nn.Linear(self.seg_len, self.d_model),
+            nn.ReLU()
+        )
+        self.rnn = nn.GRU(input_size=self.d_model, hidden_size=self.d_model, num_layers=1, bias=True,
+                              batch_first=True, bidirectional=False)
+        self.pos_emb = nn.Parameter(torch.randn(self.seg_num_y, self.d_model // 2))
+        self.channel_emb = nn.Parameter(torch.randn(self.enc_in, self.d_model // 2))
+
+        self.predict = nn.Sequential(
+            nn.Dropout(self.dropout),
+            nn.Linear(self.d_model, self.seg_len)
+        )
+
+    def encoder(self, x):
+        # b:batch_size c:channel_size s:seq_len
+        # d:d_model w:seg_len n:seg_num_x m:seg_num_y
+        batch_size = x.size(0)
+
+        # normalization and permute     b,s,c -> b,c,s
+        seq_last = x[:, -1:, :].detach()
+        x = (x - seq_last) # b,c,s
+
+        # segment and embedding    b,c,s -> bc,n,w -> bc,n,d
+        x = self.valueEmbedding(x.reshape(-1, self.seg_num_x, self.seg_len))
+
+        # encoding
+        _, hn = self.rnn(x) # bc,n,d  1,bc,d
+
+        # m,d//2 -> 1,m,d//2 -> c,m,d//2
+        # c,d//2 -> c,1,d//2 -> c,m,d//2
+        # c,m,d -> cm,1,d -> bcm, 1, d
+        pos_emb = torch.cat([
+            self.pos_emb.unsqueeze(0).repeat(self.enc_in, 1, 1),
+            self.channel_emb.unsqueeze(1).repeat(1, self.seg_num_y, 1)
+        ], dim=-1).view(-1, 1, self.d_model).repeat(batch_size,1,1)
+
+        _, hy = self.rnn(pos_emb, hn.repeat(1, 1, self.seg_num_y).view(1, -1, self.d_model)) # bcm,1,d  1,bcm,d
+
+        # 1,bcm,d -> 1,bcm,w -> b,c,s
+        y = self.predict(hy).view(-1, self.enc_in, self.pred_len)
+
+        # permute and denorm
+        y = y.permute(0, 2, 1) + seq_last
+        return y
+
+    def forecast(self, x_enc):
+        # Encoder
+        return self.encoder(x_enc)
+
+    def forward(self,feat, extra_feat=None):
+        x = feat.unsqueeze(-1)
+        if extra_feat is not None:
+            x = torch.cat([feat.unsqueeze(-1), extra_feat], dim=-1)
+        B, node, seq_len, channel = x.shape
+        x_enc = x.view(B * node, seq_len, channel)
+        dec_out = self.forecast(x_enc)
+        features = dec_out[:, -1, :]
+        out = features.view(B, node)
+        return out
