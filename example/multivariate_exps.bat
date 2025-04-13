2c7e547667a631763e7f0440a26d784199c649aa
MatZaharia <guozh29@mail2.sysu.edu.cn>
2025-04-13 19:02:59 +0800

    Create multivariate_exps.bat

---
+++ example/multivariate_exps.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set cities=AMS SZH SPO JHB LOA MEL
+set auxiliary=all e_price s_price temp precip visibility
+
+for %%c in (%cities%) do (
+    for %%a in (%auxiliary%) do (
+            python multivariate_prediction.py --city %%c --model frets --output_path ./result/multivariate/ frets --auxiliary %%a|| (
+                echo Error occurred on city %%c, auxiliary %%a
+                pause
+                exit /b 1
+            )
+        )
+)
+echo All experiments completed.
