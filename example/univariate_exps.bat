cdf592bbfc75f02ee7f9d97f9544311c39519b5a
GZH <99848958+MatZaharia@users.noreply.github.com>
2025-04-13 15:36:59 +0800

    Update univariate_exps.bat

--- example/univariate_exps.bat
+++ example/univariate_exps.bat
@@ -1,11 +1,14 @@
 @echo off
 
-set cities=SPO AMS JHB LOA MEL SZH
-set models=arima ar lo fcnn lstm
-set folds=1 2 3 4 5 6
+set cities=AMS SZH SPO JHB LOA MEL
+@REM set cities=AMS
+@REM set models=arima ar lo fcnn lstm segrnn frets moderntcn multipatchformer
+set models=moderntcn multipatchformer
+@REM set folds=1 2 3 4 5 6
+set folds=6 5 4 3 2 1
 
-for %%c in (%cities%) do (
-    for %%m in (%models%) do (
+for %%m in (%models%) do (
+    for %%c in (%cities%) do (
         for %%f in (%folds%) do (
                 python univariate_prediction.py --city %%c --model %%m --fold %%f|| (
                     echo Error occurred on city %%c, model %%m, fold %%f
