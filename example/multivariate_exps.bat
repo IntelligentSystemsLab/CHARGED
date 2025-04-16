2e40755d6cebf1b809f314c2229e5218230e69b0
GZH <guozh29@mail2.sysu.edu.cn>
2025-04-16 13:02:40 +0800

    Update multivariate_exps.bat

--- example/multivariate_exps.bat
+++ example/multivariate_exps.bat
@@ -5,7 +5,7 @@ set auxiliary=all e_price s_price temp precip visibility
 
 for %%c in (%cities%) do (
     for %%a in (%auxiliary%) do (
-            python multivariate_prediction.py --city %%c --model frets --output_path ./result/multivariate/ frets --auxiliary %%a|| (
+            python multivariate_prediction.py --city %%c --model frets --output_path ./result/multivariate/ --auxiliary %%a|| (
                 echo Error occurred on city %%c, auxiliary %%a
                 pause
                 exit /b 1
